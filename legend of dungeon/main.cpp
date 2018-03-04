74477995396fa31133059fdd7fd0788c25cd50bf
Joel Huffman <Jhuff@joels-mbp.home>
2018-03-04 15:58:36 -0800

    Initial Commit

---
+++ legend of dungeon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  legend of dungeon
+//
+//  Created by Joel Huffman on 3/4/18.
+//  Copyright © 2018 Joel Huffman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
